a2212616bdd596162504a697c95f383c9d653393
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-18 18:25:17 +0400

    logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,17 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Header() {
   return (
-    <div>
+    <div className="fixed top-0">
+      <nav>
+        <Link href={"/"} className="flex gap-2 items-center justify-center">
+          <Image src={"/logo.png"} alt="logo" width={40} height={40} />
+          <h2>Wealth</h2>
+        </Link>
+      </nav>
       <SignedOut>
         <SignInButton />
       </SignedOut>
