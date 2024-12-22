5ae381bae3c1e3a9876e071b530bdad2c6f2eb1e
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-22 15:54:03 +0400

    prisma db

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import HeaderButton from "./HeaderButton";
+import { checkUser } from "@/lib/CheckUser";
 
-function Header() {
+async function Header() {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b shadow-md">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between ">
